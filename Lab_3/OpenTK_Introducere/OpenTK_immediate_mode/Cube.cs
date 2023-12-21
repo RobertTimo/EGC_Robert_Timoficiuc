e723cd04b9fb531f39cdf4fe27587e1bf0ec4f83
RobertTimo <135763891+RobertTimo@users.noreply.github.com>
2023-12-20 21:43:58 -0800

    Laborator 3

---
+++ Lab_3/OpenTK_Introducere/OpenTK_immediate_mode/Cube.cs
@@ -0,0 +1,187 @@
+﻿
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing;
+
+namespace OpenTK_immediate_mode
+{
+    public class Cube
+    {
+        private bool myVisibility;
+
+        private bool hideFace1;
+        private bool hideFace2;
+        private bool hideFace3;
+        private bool hideFace4;
+
+        private readonly int changeY = 1;
+
+        private PolygonMode myPolygonMode;
+
+        private Color colorBaseCube = Color.Azure;
+        private Color colorFaceCube1 = Color.BurlyWood;
+        private Color colorFaceCube2 = Color.Salmon;
+        private Color colorFaceCube3 = Color.Maroon;
+        private Color colorFaceCube4 = Color.Plum;
+
+        private List<Vector3> baseList = new List<Vector3>
+        {
+            new Vector3(30,0,-30),
+            new Vector3(30,0,30),
+            new Vector3(-30,0,30),
+            new Vector3(-30,0,-30),
+
+            new Vector3(30,60,-30),
+            new Vector3(30,60,30),
+            new Vector3(-30,60,30),
+            new Vector3(-30,60,-30)
+
+        };
+
+        public Cube()
+        {
+            myVisibility = true;
+
+            hideFace1 = true;
+            hideFace2 = true;
+            hideFace3 = true;
+            hideFace4 = true;
+
+            myPolygonMode = PolygonMode.Fill;
+        }
+
+        public void Draw()
+        {
+            if (myVisibility)
+            {
+                GL.PolygonMode(MaterialFace.FrontAndBack, myPolygonMode);
+
+                GL.Begin(PrimitiveType.Quads);
+
+                GL.Color3(colorBaseCube);
+
+                foreach (Vector3 vector in baseList)
+                {
+                    GL.Vertex3(vector);
+                }
+
+                if (hideFace1)
+                {
+                    GL.Color3(colorFaceCube1);
+
+                    GL.Vertex3(baseList[2]);
+                    GL.Vertex3(baseList[3]);
+                    GL.Vertex3(baseList[7]);
+                    GL.Vertex3(baseList[6]);
+                }
+
+                if (hideFace2)
+                {
+                    GL.Color3(colorFaceCube2);
+
+                    GL.Vertex3(baseList[3]);
+                    GL.Vertex3(baseList[0]);
+                    GL.Vertex3(baseList[4]);
+                    GL.Vertex3(baseList[7]);
+                }
+
+                if (hideFace3)
+                {
+                    GL.Color3(colorFaceCube3);
+
+                    GL.Vertex3(baseList[0]);
+                    GL.Vertex3(baseList[1]);
+                    GL.Vertex3(baseList[5]);
+                    GL.Vertex3(baseList[4]);
+                }
+
+                if (hideFace4)
+                {
+                    GL.Color3(colorFaceCube4);
+
+                    GL.Vertex3(baseList[1]);
+                    GL.Vertex3(baseList[2]);
+                    GL.Vertex3(baseList[6]);
+                    GL.Vertex3(baseList[5]);
+                }
+
+                GL.End();
+            }
+        }
+
+        public void MoveUp()
+        {
+            for (int i = 0; i < baseList.Count; i++)
+            {
+                baseList[i] = new Vector3(baseList[i].X, baseList[i].Y + changeY, baseList[i].Z);
+            }
+        }
+
+        public void MoveDown()
+        {
+            for (int i = 0; i < baseList.Count; i++)
+            {
+                baseList[i] = new Vector3(baseList[i].X, baseList[i].Y - changeY, baseList[i].Z);
+            }
+        }
+
+        public void ChangeComposition()
+        {
+            if (myPolygonMode == PolygonMode.Fill)
+            {
+                myPolygonMode = PolygonMode.Line;
+            }
+            else if (myPolygonMode == PolygonMode.Line)
+            {
+                myPolygonMode = PolygonMode.Point;
+            }
+            else
+            {
+                myPolygonMode = PolygonMode.Fill;
+            }
+        }
+
+        public void Show()
+        {
+            myVisibility = true;
+        }
+
+        /// <summary>
+        /// Sets visibility of the object OFF.
+        /// </summary>
+        public void Hide()
+        {
+            myVisibility = false;
+        }
+
+        /// <summary>
+        /// Toggles the myVisibility of the object. Once triggered, the attribute is applied automatically on drawing.
+        /// </summary>
+        public void ToggleVisibility()
+        {
+            myVisibility = !myVisibility;
+        }
+        public void ToggleVisibility1()
+        {
+            hideFace1 = !hideFace1;
+        }
+
+        public void ToggleVisibility2()
+        {
+            hideFace2 = !hideFace2;
+        }
+
+        public void ToggleVisibility3()
+        {
+            hideFace3 = !hideFace3;
+        }
+
+        public void ToggleVisibility4()
+        {
+            hideFace4 = !hideFace4;
+        }
+    }
+}
---
+++ Lab_3/OpenTK_Introducere/OpenTK_immediate_mode/ImmediateMode.cs
@@ -0,0 +1,154 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+using System;
+using System.Drawing;
+
+namespace OpenTK_immediate_mode
+{
+    class Window3D : GameWindow
+    {
+        private KeyboardState previousKeyboard;
+        private MouseState previousMouse;
+        private readonly Axes ax;
+        private readonly Cube cube;
+
+        // DEFAULTS
+        private readonly Color DEFAULT_BKG_COLOR = Color.FromArgb(49, 50, 51);
+        private Vector3 eye = new Vector3(200, 200, 200);
+        private Vector3 target = new Vector3(0, 0, 0);
+        private Vector3 up_vector = new Vector3(0, 1, 0);
+        private Random random = new Random();
+
+        public Window3D() : base(800, 800, new GraphicsMode(32, 24, 0, 8))
+        {
+            VSync = VSyncMode.On;
+
+            Title = "Timoficiuc Robert-Mihai. grupa 3133A";
+
+            // inits
+            ax = new Axes();
+            cube = new Cube();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            GL.Enable(EnableCap.DepthTest);
+            GL.DepthFunc(DepthFunction.Less);
+
+            GL.Hint(HintTarget.PolygonSmoothHint, HintMode.Nicest);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            // set background
+            GL.ClearColor(DEFAULT_BKG_COLOR);
+
+            // set viewport
+            GL.Viewport(0, 0, this.Width, this.Height);
+
+            // set perspective
+            Matrix4 perspectiva = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, (float)this.Width / (float)this.Height, 1, 512);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref perspectiva);
+
+            // set the eye
+            Matrix4 camera = Matrix4.LookAt(eye, target, up_vector);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref camera);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            // LOGIC CODE
+            KeyboardState currentKeyboard = Keyboard.GetState();
+            MouseState currentMouse = Mouse.GetState();
+
+            if (currentKeyboard[Key.Escape])
+            {
+                Exit();
+            }
+
+            if (currentKeyboard[Key.W])
+            {
+                cube.MoveUp();
+            }
+
+            if (currentKeyboard[Key.S])
+            {
+                cube.MoveDown();
+            }
+
+            if (currentKeyboard[Key.B] && !previousKeyboard[Key.B])
+            {
+                ChangeBgColor(random);
+            }
+
+            if (currentKeyboard[Key.C] && !previousKeyboard[Key.C])
+            {
+                cube.ChangeComposition();
+            }
+
+            if (currentKeyboard[Key.V] && !previousKeyboard[Key.V])
+            {
+                cube.ToggleVisibility();
+            }
+
+            if (currentKeyboard[Key.Z] && !previousKeyboard[Key.Z])
+            {
+                cube.ToggleVisibility1();
+            }
+
+            if (currentKeyboard[Key.X] && !previousKeyboard[Key.X])
+            {
+                cube.ToggleVisibility2();
+            }
+
+            if (currentKeyboard[Key.N] && !previousKeyboard[Key.N])
+            {
+                cube.ToggleVisibility3();
+            }
+
+            if (currentKeyboard[Key.M] && !previousKeyboard[Key.M])
+            {
+                cube.ToggleVisibility4();
+            }
+
+            previousKeyboard = currentKeyboard;
+            previousMouse = currentMouse;
+            // END logic code
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Clear(ClearBufferMask.DepthBufferBit);
+
+            // RENDER AXES
+            ax.Draw();
+
+            // RENDER CUBE
+            cube.Draw();
+
+
+            // END render code
+
+            SwapBuffers();
+        }
+
+        private void ChangeBgColor(Random random)
+        {
+            GL.ClearColor(Color.FromArgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 225)));
+        }
+    }
+}
